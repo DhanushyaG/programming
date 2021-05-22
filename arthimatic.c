7c370bf626fd964ef5a687e15427913b90159010
DhanushyaG <84453418+DhanushyaG@users.noreply.github.com>
2021-05-22 16:47:11 +0530

    Create arthimatic.c

---
+++ arthimatic.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{
+  int a=4,b=2;
+  printf("%d+%d=%d\n",a,b,a+b);
+  printf("%d-%d=%d\n",a,b,a-b);
+  printf("%d*%d=%d\n",a,b,a*b);
+  printf("%d/%d=%d\n",a,b,a/b);
+  printf("%d%%d=%d\n",a,b,a%b);
+}
