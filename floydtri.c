6c6b49afca2ce649c77386583bf0a4fc6716ab99
DhanushyaG <84453418+DhanushyaG@users.noreply.github.com>
2021-07-19 15:51:41 +0530

    Create floydtri.c

---
+++ floydtri.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,n,sum=1;
+  printf("enter number of rows:");
+  scanf("%d",&n;i++)
+  {
+    for(j=1;j<=i;j++)
+    {
+      pri ntf("%d",sum);
+      sum++;
+    }
+    printf("/n");
+  }
+  return 0;
+}
